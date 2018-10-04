6b521167396f21ba52068e4815da48fb1eb4da3c
dmt <danielt@alleninstitute.org>
2018-10-04 14:39:15 -0700

    remove subdirectory from legacy image server path

--- src/aics-image-viewer/shared/constants.js
+++ src/aics-image-viewer/shared/constants.js
@@ -15,9 +15,9 @@ export const
 
   ISOSURFACE_OPACITY_SLIDER_MAX = 255.0,
 
-  // TODO: fix these server names.  Only LEGACY_IMAGE_SERVER is correct and only for a small subset of the data.
+  // TODO: fix these server names.  Only LEGACY_IMAGE_SERVER is correct and only if the dataset subdirectory name is known.
   LEGACY_DOWNLOAD_SERVER = 'http://downloads.allencell.org/1.2.0/Cell-Viewer_Data/',
-  LEGACY_IMAGE_SERVER = 'https://cellviewer-1-2-0.allencell.org/aics/thumbnails/2017_03_08_Struct_First_Pass_Seg/',
+  LEGACY_IMAGE_SERVER = 'https://cellviewer-1-2-0.allencell.org/aics/thumbnails/',
 
   DOWNLOAD_SERVER = 'http://dev-aics-dtp-001/cellviewer-1-3-0/Cell-Viewer_Data/',
   IMAGE_SERVER = 'http://dev-aics-dtp-001/cellviewer-1-3-0/Cell-Viewer_Thumbnails/';
