378e3638c6bdfaf47f6d60f1e1ac0b5d9ffd9e18
dmt <danielt@alleninstitute.org>
2018-10-22 18:09:35 -0700

    parameterize servers for dev, staging, and production.  server names will be picked up by environment but have not been differentiated yet.

--- webpack.dev.js
+++ webpack.dev.js
@@ -17,7 +17,9 @@ module.exports = merge(common, {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        'IMAGE_VIEWER_SERVICE_URL': JSON.stringify('//allen/aics/animated-cell/Allen-Cell-Explorer/Allen-Cell-Explorer_1.3.0')
+        'IMAGE_VIEWER_SERVICE_URL': JSON.stringify('//allen/aics/animated-cell/Allen-Cell-Explorer/Allen-Cell-Explorer_1.3.0'),
+        'DOWNLOAD_SERVER': JSON.stringify('http://dev-aics-dtp-001/cellviewer-1-3-0/Cell-Viewer_Data/'),
+        'IMAGE_SERVER': JSON.stringify('http://dev-aics-dtp-001/cellviewer-1-3-0/Cell-Viewer_Thumbnails/')
       }
     })
   ]
