feebc2cb5020b97178254d8af2de04a26c1d90c5
Dan Toloudis <danielt@alleninstitute.org>
2018-12-07 17:09:37 -0800

    fix spelling of TL 100x channel

--- src/aics-image-viewer/shared/enums/channelGroups.js
+++ src/aics-image-viewer/shared/enums/channelGroups.js
@@ -5,7 +5,7 @@ import {
 } from '../constants';
 
 export const channelGroupingMap = {
-    [OBSERVED_CHANNEL_KEY]: ['CMDRP', 'EGFP', 'mtagRFPT', 'H3342', 'Bright_100', 'Bright_100X', 'TL 100X', ],
+    [OBSERVED_CHANNEL_KEY]: ['CMDRP', 'EGFP', 'mtagRFPT', 'H3342', 'Bright_100', 'Bright_100X', 'TL 100x', ],
     [SEGMENTATION_CHANNEL_KEY]: ['SEG_STRUCT', 'SEG_Memb', 'SEG_DNA'],
     [CONTOUR_CHANNEL_KEY]: ['CON_Memb', 'CON_DNA']
 };
