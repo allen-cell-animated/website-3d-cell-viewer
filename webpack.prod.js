150b72945f2d1657f73626b8e45e2d1e1976d459
Megan Riel-Mehan <meganrm@gmail.com>
2018-12-10 18:46:56 -0800

    updated build settings

--- webpack.prod.js
+++ webpack.prod.js
@@ -9,7 +9,7 @@ module.exports = merge(common, {
       'process.env': {
         'IMAGE_VIEWER_SERVICE_URL': JSON.stringify('//allen/aics/animated-cell/Allen-Cell-Explorer/Allen-Cell-Explorer_1.3.0'),
         'DOWNLOAD_SERVER': JSON.stringify('http://dev-aics-dtp-001/cellviewer-1-3-0/Cell-Viewer_Data/'),
-        'IMAGE_SERVER': JSON.stringify('http://dev-aics-dtp-001/cellviewer-1-3-0/Cell-Viewer_Thumbnails/')
+        'IMAGE_SERVER': JSON.stringify('https://s3-us-west-2.amazonaws.com/bisque.allencell.org/v1.3.0/Cell-Viewer_Thumbnails/')
       }
     })
   ]
