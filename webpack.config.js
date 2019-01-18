c221bffb2ea799e8c9e9f32798d7d80637e392d7
Megan Riel-Mehan <meganrm@gmail.com>
2019-01-17 16:57:44 -0800

    reverting webpack config setting back

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const stage = require('./webpack.stage');
 const dev = require('./webpack.dev');
 
 module.exports = () => {
-  const DEPLOYMENT_ENV = 'production';
+  const DEPLOYMENT_ENV = process.env.DEPLOYMENT_ENV || 'dev';
   console.log(`Deployment environment is: ${DEPLOYMENT_ENV}`);
   switch(DEPLOYMENT_ENV) {
     case 'production':
