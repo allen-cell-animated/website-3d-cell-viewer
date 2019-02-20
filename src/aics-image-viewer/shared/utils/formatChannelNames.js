291d00c7ecb7ae2f30f35978cc22e8fd14b64054
Megan Riel-Mehan <meganrm@gmail.com>
2019-02-20 12:17:37 -0800

    added new dna channel name

--- src/aics-image-viewer/shared/utils/formatChannelNames.js
+++ src/aics-image-viewer/shared/utils/formatChannelNames.js
@@ -1,10 +1,12 @@
 export default function formatChannelNames(channelName, structureName) {
-    if (channelName.split('_').length > 1) {
+
+    if (channelName.includes('CON_') || channelName.includes('SEG_')) {
         const splitName = channelName.split('_')[1];
         const mapping = {
             Memb: 'Membrane',
             STRUCT: structureName || 'Labeled structure',
             DNA: 'DNA',
+            H3342: 'DNA',
             '100X': 'Bright field',
             '100': 'Bright field'
         };
@@ -15,7 +17,9 @@ export default function formatChannelNames(channelName, structureName) {
         EGFP: structureName || 'Labeled structure',
         mtagRFPT: structureName || 'Labeled structure',
         H3342: 'DNA',
-        'TL 100x': 'Bright field'
+        H3342_3: 'DNA',
+        Bright_100X: 'Bright field',
+        'TL 100x': 'Bright field',
     };
     return mapping[channelName] ? mapping[channelName] : channelName;
 };
