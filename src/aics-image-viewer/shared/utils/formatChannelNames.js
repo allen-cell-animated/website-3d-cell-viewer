e3fbc3d592d797d436a3976b37a6e6df74f8a612
Megan Riel-Mehan <meganrm@gmail.com>
2019-02-27 15:26:51 -0800

    simplify channel name check with regex

--- src/aics-image-viewer/shared/utils/formatChannelNames.js
+++ src/aics-image-viewer/shared/utils/formatChannelNames.js
@@ -1,25 +1,19 @@
-export default function formatChannelNames(channelName, structureName) {
+import { find } from 'lodash';
 
-    if (channelName.includes('CON_') || channelName.includes('SEG_')) {
-        const splitName = channelName.split('_')[1];
-        const mapping = {
-            Memb: 'Membrane',
-            STRUCT: structureName || 'Labeled structure',
-            DNA: 'DNA',
-            H3342: 'DNA',
-            '100X': 'Bright field',
-            '100': 'Bright field'
-        };
-        return mapping[splitName] ? mapping[splitName] : splitName;
-    }
-    const mapping = {
-        CMDRP: 'Membrane',
-        EGFP: structureName || 'Labeled structure',
-        mtagRFPT: structureName || 'Labeled structure',
-        H3342: 'DNA',
-        H3342_3: 'DNA',
-        Bright_100X: 'Bright field',
-        'TL 100x': 'Bright field',
-    };
-    return mapping[channelName] ? mapping[channelName] : channelName;
+export default function formatChannelNames(channelName, structureName) {
+    const membraneRegEx = /(CMDRP)|(Memb)/g;
+    const structureRegEx = /(EGFP)|(RFPT)|(STRUCT)/g;
+    const dnaRegEx = /(H3342)|(DNA)/g;
+    const brightFieldRegEx = /(100)|(Bright)/g;
+
+    const mapping = [
+        {test: membraneRegEx, label: 'Membrane'},
+        { test: structureRegEx, label: structureName || 'Labeled structure'},
+        { test: dnaRegEx, label: 'DNA' },
+        { test: brightFieldRegEx, label: 'Bright field' },
+    ];
+    const toReturn = find(mapping, (value) => {
+        return value.test.test(channelName);
+    });
+    return toReturn ? toReturn.label : channelName;
 };
