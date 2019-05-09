3ad37951028473e113d02f8234d1bc4ae34ce75f
Dan Toloudis <danielt@alleninstitute.org>
2019-05-09 16:56:31 -0700

    update dev server test app

--- public/index.jsx
+++ public/index.jsx
@@ -8,13 +8,19 @@ import "./App.scss";
 import { ImageViewerApp } from "../src";
 
 ReactDOM.render(
-  <ImageViewerApp
-    cellId={23618}
-    baseUrl="http://dev-aics-dtp-001.corp.alleninstitute.org/cellviewer-1-4-0/Cell-Viewer_Thumbnails/"
-    cellPath="AICS-17/AICS-17_4187_23618"
-    fovPath="AICS-17/AICS-17_4187"
-    defaultVolumesOn={[0, 1, 2]}
-    defaultSurfacesOn={[]}
-  />,
+  <section className="ant-layout">
+    <div className="mycellviewer">
+      <ImageViewerApp
+        cellId={23618}
+        baseUrl="http://dev-aics-dtp-001.corp.alleninstitute.org/cellviewer-1-4-0/Cell-Viewer_Thumbnails/"
+        //cellPath="AICS-25/AICS-25_6035_43757"
+        //fovPath="AICS-25/AICS-25_6035"
+        cellPath="AICS-17/AICS-17_4187_23618"
+        fovPath="AICS-17/AICS-17_4187"
+        defaultVolumesOn={[0, 1, 2]}
+        defaultSurfacesOn={[]}
+      />
+    </div>
+  </section>,
   document.getElementById("cell-viewer")
 );
