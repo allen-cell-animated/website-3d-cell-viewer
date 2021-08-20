6b2b53346c3d9ba54a6e22884205973361b1e34a
Dan <danielt@alleninstitute.org>
2021-08-20 15:59:56 -0700

    more toolchain updates

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
@@ -15,7 +15,7 @@ module.exports = {
     extensions: ['.js', '.jsx', '.ts', '.tsx']
   },
   plugins: [
-    new CleanWebpackPlugin(['imageviewer/*']),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './public/index.html'
     }),
--- webpack.library.js
+++ webpack.library.js
@@ -1,141 +1,123 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: path.resolve(__dirname, "src/index.js"),
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-    library: 'AicsImageViewer',
-    libraryTarget: 'commonjs2'
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    library: {
+      type: "umd"
+    },
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
-  externals: [{
+  externals: [
+    {
       react: {
-        root: 'React',
-        commonjs2: 'react',
-        commonjs: 'react',
-        amd: 'react'
-      }
+        root: "React",
+        commonjs2: "react",
+        commonjs: "react",
+        amd: "react",
+      },
     },
     {
-      'react-dom': {
-        root: 'ReactDOM',
-        commonjs2: 'react-dom',
-        commonjs: 'react-dom',
-        amd: 'react-dom'
-      }
+      "react-dom": {
+        root: "ReactDOM",
+        commonjs2: "react-dom",
+        commonjs: "react-dom",
+        amd: "react-dom",
+      },
     },
-    {
-      'antd': {
-        root: 'antd',
-        commonjs2: 'antd',
-        commonjs: 'antd',
-        amd: 'antd'
-      }
-    },
-    {
-      'prop-types': {
-        root: 'PropTypes',
-        commonjs2: 'prop-types',
-        commonjs: 'prop-types',
-        amd: 'prop-types'
-      }
-    }
   ],
   plugins: [
-    new CleanWebpackPlugin(['dist/*']),
-    new webpack.ProvidePlugin({
-      THREE: 'three',
-      jQuery: 'jquery',
-      $: 'jquery'
-    }),
+    new CleanWebpackPlugin(),
     new webpack.DefinePlugin({
-      'process.env': {
-        'IMAGE_VIEWER_SERVICE_URL': JSON.stringify('//allen/aics/animated-cell/Allen-Cell-Explorer/Allen-Cell-Explorer_1.3.0'),
-        'DOWNLOAD_SERVER': JSON.stringify('http://dev-aics-dtp-001/cellviewer-1-3-0/Cell-Viewer_Data/'),
-        'IMAGE_SERVER': JSON.stringify('https://s3-us-west-2.amazonaws.com/bisque.allencell.org/v1.3.0/Cell-Viewer_Thumbnails/')
-      }
-    })
+      "process.env": {
+        IMAGE_VIEWER_SERVICE_URL: JSON.stringify(
+          "//allen/aics/animated-cell/Allen-Cell-Explorer/Allen-Cell-Explorer_1.3.0"
+        ),
+        DOWNLOAD_SERVER: JSON.stringify("http://dev-aics-dtp-001/cellviewer-1-3-0/Cell-Viewer_Data/"),
+        IMAGE_SERVER: JSON.stringify(
+          "https://s3-us-west-2.amazonaws.com/bisque.allencell.org/v1.3.0/Cell-Viewer_Thumbnails/"
+        ),
+      },
+    }),
   ],
-  devtool: 'cheap-module-source-map',
+  devtool: "cheap-module-source-map",
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.(js|jsx|ts|tsx)$/,
-        include: [
-          path.resolve(__dirname, 'src')
-        ],
+        include: [path.resolve(__dirname, "src")],
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: "babel-loader",
       },
       {
         test: /\.scss$/,
         use: [
-          'style-loader',
+          "style-loader",
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
-              sourceMap: true
-            }
+              sourceMap: true,
+            },
           },
-          'resolve-url-loader',
+          "resolve-url-loader",
           {
-            loader: 'sass-loader',
+            loader: "sass-loader",
             options: {
               sourceMap: true,
               sassOptions: {
-                includePaths: [`${__dirname}/src/aics-image-viewer/assets/styles`]
-              }
-            }
-          }
-        ]
+                includePaths: [`${__dirname}/src/aics-image-viewer/assets/styles`],
+              },
+            },
+          },
+        ],
       },
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'resolve-url-loader',
-          'css-loader'
-        ]
+        use: ["style-loader", "resolve-url-loader", "css-loader"],
       },
       {
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: /node_modules/,
         test: /\.(js|jsx|ts|tsx)$/,
       },
       {
         test: /\.less$/,
         use: [
-          'style-loader',
+          "style-loader",
           {
             loader: "css-loader",
             options: {
               camelCase: true,
-              importLoaders: 1
-            }
+              importLoaders: 1,
+            },
           },
           {
             loader: "less-loader",
             options: {
               javascriptEnabled: true,
-            }
-          }
-        ]
+            },
+          },
+        ],
       },
       {
         test: /\.(woff|woff2|tff|eot|glyph|svg)$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            name: 'imageviewer/font/[name].[ext]'
-          }
-        }]
-      }
-    ]
-  }
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10000,
+              name: "imageviewer/font/[name].[ext]",
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
