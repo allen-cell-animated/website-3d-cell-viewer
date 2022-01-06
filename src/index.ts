747e3151b80051e347d1a769c2f84597218b570f
toloudis <danielt@alleninstitute.org>
2022-01-06 15:13:16 -0800

    export new settings types

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import ImageViewerApp from "./aics-image-viewer/components/App";
+
+export type {
+  ViewerChannelSettings,
+  ViewerChannelGroup,
+  ViewerChannelSetting,
+} from "./aics-image-viewer/shared/utils/initialViewerSettings";
+
+export { ImageViewerApp };
