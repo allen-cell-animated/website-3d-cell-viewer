d0e99b4ab263e8c2e91ba00621d14849d75bb925
toloudis <danielt@alleninstitute.org>
2022-01-11 15:25:50 -0800

    move props and state declarations into a separate file

--- src/aics-image-viewer/components/App/index.tsx
+++ src/aics-image-viewer/components/App/index.tsx
@@ -2,26 +2,16 @@
 import { Layout, Progress } from "antd";
 import React from "react";
 import { includes, isEqual, find, map } from "lodash";
-import {
-  RENDERMODE_PATHTRACE,
-  RENDERMODE_RAYMARCH,
-  View3d,
-  Volume,
-  VolumeLoader,
-  ImageInfo,
-  Lut,
-} from "@aics/volume-viewer";
+import { RENDERMODE_PATHTRACE, RENDERMODE_RAYMARCH, Volume, VolumeLoader, Lut } from "@aics/volume-viewer";
 
+import { AppProps, AppState, UserSelectionState } from "./types";
 import { controlPointsToLut } from "../../shared/utils/controlPointsToLut";
 import HttpClient from "../../shared/utils/httpClient";
-import {
-  findFirstChannelMatch,
-  makeChannelIndexGrouping,
-  ViewerChannelSettings,
-} from "../../shared/utils/viewerChannelSettings";
+import { findFirstChannelMatch, makeChannelIndexGrouping } from "../../shared/utils/viewerChannelSettings";
 import enums from "../../shared/enums";
 import {
   CELL_SEGMENTATION_CHANNEL_NAME,
+  CHANNEL_SETTINGS,
   PRESET_COLORS_0,
   ALPHA_MASK_SLIDER_3D_DEFAULT,
   ALPHA_MASK_SLIDER_2D_DEFAULT,
@@ -72,7 +62,6 @@ const { Sider, Content } = Layout;
 const OK_STATUS = "OK";
 const ERROR_STATUS = "Error";
 const INIT_COLORS = PRESET_COLORS_0;
-const CHANNEL_SETTINGS = "channelSettings";
 
 function colorHexToArray(hex) {
   // hex is a xxxxxx string. split it into array of rgb ints
@@ -82,106 +71,6 @@ function colorHexToArray(hex) {
   } else {
     return null;
   }
-}
-
-interface AppProps {
-  // rawData has a "dtype" which is expected to be "uint8", a "shape":[c,z,y,x] and a "buffer" which is a DataView
-  rawData?: { dtype: "uint8"; shape: [number, number, number, number]; buffer: DataView };
-  // rawDims is the volume dims that normally come from a json file (see handleOpenImageResponse)
-  rawDims?: ImageInfo;
-
-  // replaces / obviates groupToChannelNameMap, channelNameClean, channelNameMapping, filterFunc, initialChannelSettings, defaultSurfacesOn and defaultVolumesOn
-  viewerChannelSettings?: ViewerChannelSettings;
-
-  maskChannelName: string;
-
-  appHeight: string;
-  cellId: string;
-  cellPath: string;
-  fovPath: string;
-  renderConfig: {
-    alphaMask: boolean;
-    autoRotateButton: boolean;
-    axisClipSliders: boolean;
-    brightnessSlider: boolean;
-    colorPicker: boolean;
-    colorPresetsDropdown: boolean;
-    densitySlider: boolean;
-    levelsSliders: boolean;
-    saveSurfaceButtons: boolean;
-    fovCellSwitchControls: boolean;
-    viewModeRadioButtons: boolean;
-  };
-  viewerConfig: {
-    view: string; // "3D", "XY", "XZ", "YZ"
-    mode: string; // "default", "pathtrace", "maxprojection"
-    maskAlpha: number; //ALPHA_MASK_SLIDER_3D_DEFAULT[0],
-    brightness: number; //BRIGHTNESS_SLIDER_LEVEL_DEFAULT[0],
-    density: number; //DENSITY_SLIDER_LEVEL_DEFAULT[0],
-    levels: [number, number, number]; // LEVELS_SLIDER_DEFAULT,
-    region?: [number, number, number, number, number, number]; //[0,1,0,1,0,1], // or ignored if slice is specified with a non-3D mode
-    slice?: number; // or integer slice to show in view mode XY, YZ, or XZ.  mut. ex with region
-  };
-  baseUrl: string;
-  nextImgPath: string;
-  prevImgPath: string;
-  cellDownloadHref: string;
-  fovDownloadHref: string;
-  preLoad: boolean;
-  pixelSize?: [number, number, number];
-  canvasMargin: string;
-}
-
-interface UserSelectionState {
-  imageType: string; // SEGMENTED_CELL | FULL_FIELD_IMAGE,
-  controlPanelClosed: boolean;
-  [MODE]: symbol;
-  [AUTO_ROTATE]: boolean;
-  [MAX_PROJECT]: boolean;
-  [PATH_TRACE]: boolean;
-  [ALPHA_MASK_SLIDER_LEVEL]: number[]; //[props.viewerConfig.maskAlpha] || ALPHA_MASK_SLIDER_3D_DEFAULT,
-  [BRIGHTNESS_SLIDER_LEVEL]: number[]; //[props.viewerConfig.brightness] || BRIGHTNESS_SLIDER_LEVEL_DEFAULT,
-  [DENSITY_SLIDER_LEVEL]: number[]; // [props.viewerConfig.density] || DENSITY_SLIDER_LEVEL_DEFAULT,
-  [LEVELS_SLIDER]: [number, number, number]; //props.viewerConfig.levels || LEVELS_SLIDER_DEFAULT,
-  // channelSettings is a flat list of objects of this type:
-  // { name, enabled, volumeEnabled, isosurfaceEnabled, isovalue, opacity, color, dataReady}
-  // the list is in the order they were in the raw data.
-  [CHANNEL_SETTINGS]: {
-    name: string;
-    enabled: boolean;
-    volumeEnabled: boolean;
-    isosurfaceEnabled: boolean;
-    isovalue: number;
-    opacity: number;
-    color: string;
-    dataReady: boolean;
-    [LUT_CONTROL_POINTS]: [];
-  }[];
-}
-
-interface AppState {
-  view3d: View3d | null;
-  image: Volume | null;
-  nextImg: Volume | null;
-  prevImg: Volume | null;
-  files: null;
-  cellId?: string;
-  fovPath: string;
-  cellPath: string;
-  queryErrorMessage: null | string;
-  sendingQueryRequest: boolean;
-  openFilesOnly: boolean;
-  channelDataReady: { [key: string]: boolean };
-  // channelGroupedByType is an object where channel indexes are grouped by type (observed, segmenations, and countours)
-  // {observed: channelIndex[], segmentations: channelIndex[], contours: channelIndex[], other: channelIndex[] }
-  channelGroupedByType: { [key: string]: number[] };
-  // did the requested image have a cell id (in queryInput)?
-  hasCellId: boolean;
-  // state set by the UI:
-  userSelections: UserSelectionState;
-  currentlyLoadedImagePath: string;
-  cachingInProgress: boolean;
-  path: string;
 }
 
 const defaultProps: AppProps = {
---
+++ src/aics-image-viewer/components/App/types.ts
@@ -0,0 +1,114 @@
+import { View3d, Volume, ImageInfo } from "@aics/volume-viewer";
+import {
+  CHANNEL_SETTINGS,
+  LUT_CONTROL_POINTS,
+  ALPHA_MASK_SLIDER_LEVEL,
+  BRIGHTNESS_SLIDER_LEVEL,
+  DENSITY_SLIDER_LEVEL,
+  LEVELS_SLIDER,
+  MODE,
+  AUTO_ROTATE,
+  MAX_PROJECT,
+  PATH_TRACE,
+} from "../../shared/constants";
+import { ViewerChannelSettings } from "../../shared/utils/viewerChannelSettings";
+
+export interface AppProps {
+  // rawData has a "dtype" which is expected to be "uint8", a "shape":[c,z,y,x] and a "buffer" which is a DataView
+  rawData?: { dtype: "uint8"; shape: [number, number, number, number]; buffer: DataView };
+  // rawDims is the volume dims that normally come from a json file (see handleOpenImageResponse)
+  rawDims?: ImageInfo;
+
+  // replaces / obviates groupToChannelNameMap, channelNameClean, channelNameMapping, filterFunc, initialChannelSettings, defaultSurfacesOn and defaultVolumesOn
+  viewerChannelSettings?: ViewerChannelSettings;
+
+  maskChannelName: string;
+
+  appHeight: string;
+  cellId: string;
+  cellPath: string;
+  fovPath: string;
+  renderConfig: {
+    alphaMask: boolean;
+    autoRotateButton: boolean;
+    axisClipSliders: boolean;
+    brightnessSlider: boolean;
+    colorPicker: boolean;
+    colorPresetsDropdown: boolean;
+    densitySlider: boolean;
+    levelsSliders: boolean;
+    saveSurfaceButtons: boolean;
+    fovCellSwitchControls: boolean;
+    viewModeRadioButtons: boolean;
+  };
+  viewerConfig: {
+    view: string; // "3D", "XY", "XZ", "YZ"
+    mode: string; // "default", "pathtrace", "maxprojection"
+    maskAlpha: number; //ALPHA_MASK_SLIDER_3D_DEFAULT[0],
+    brightness: number; //BRIGHTNESS_SLIDER_LEVEL_DEFAULT[0],
+    density: number; //DENSITY_SLIDER_LEVEL_DEFAULT[0],
+    levels: [number, number, number]; // LEVELS_SLIDER_DEFAULT,
+    region?: [number, number, number, number, number, number]; //[0,1,0,1,0,1], // or ignored if slice is specified with a non-3D mode
+    slice?: number; // or integer slice to show in view mode XY, YZ, or XZ.  mut. ex with region
+  };
+  baseUrl: string;
+  nextImgPath: string;
+  prevImgPath: string;
+  cellDownloadHref: string;
+  fovDownloadHref: string;
+  preLoad: boolean;
+  pixelSize?: [number, number, number];
+  canvasMargin: string;
+}
+
+export interface UserSelectionState {
+  imageType: string; // SEGMENTED_CELL | FULL_FIELD_IMAGE,
+  controlPanelClosed: boolean;
+  [MODE]: symbol;
+  [AUTO_ROTATE]: boolean;
+  [MAX_PROJECT]: boolean;
+  [PATH_TRACE]: boolean;
+  [ALPHA_MASK_SLIDER_LEVEL]: number[]; //[props.viewerConfig.maskAlpha] || ALPHA_MASK_SLIDER_3D_DEFAULT,
+  [BRIGHTNESS_SLIDER_LEVEL]: number[]; //[props.viewerConfig.brightness] || BRIGHTNESS_SLIDER_LEVEL_DEFAULT,
+  [DENSITY_SLIDER_LEVEL]: number[]; // [props.viewerConfig.density] || DENSITY_SLIDER_LEVEL_DEFAULT,
+  [LEVELS_SLIDER]: [number, number, number]; //props.viewerConfig.levels || LEVELS_SLIDER_DEFAULT,
+  // channelSettings is a flat list of objects of this type:
+  // { name, enabled, volumeEnabled, isosurfaceEnabled, isovalue, opacity, color, dataReady}
+  // the list is in the order they were in the raw data.
+  [CHANNEL_SETTINGS]: {
+    name: string;
+    enabled: boolean;
+    volumeEnabled: boolean;
+    isosurfaceEnabled: boolean;
+    isovalue: number;
+    opacity: number;
+    color: string;
+    dataReady: boolean;
+    [LUT_CONTROL_POINTS]: [];
+  }[];
+}
+
+export interface AppState {
+  view3d: View3d | null;
+  image: Volume | null;
+  nextImg: Volume | null;
+  prevImg: Volume | null;
+  files: null;
+  cellId?: string;
+  fovPath: string;
+  cellPath: string;
+  queryErrorMessage: null | string;
+  sendingQueryRequest: boolean;
+  openFilesOnly: boolean;
+  channelDataReady: { [key: string]: boolean };
+  // channelGroupedByType is an object where channel indexes are grouped by type (observed, segmenations, and countours)
+  // {observed: channelIndex[], segmentations: channelIndex[], contours: channelIndex[], other: channelIndex[] }
+  channelGroupedByType: { [key: string]: number[] };
+  // did the requested image have a cell id (in queryInput)?
+  hasCellId: boolean;
+  // state set by the UI:
+  userSelections: UserSelectionState;
+  currentlyLoadedImagePath: string;
+  cachingInProgress: boolean;
+  path: string;
+}
