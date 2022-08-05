9000f7092771fca31562c758fa0854a29d7691a0
toloudis <danielt@alleninstitute.org>
2022-08-05 16:24:01 -0700

    add a nightly build script

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,16 +4,12 @@ const common = require("./webpack.common");
 module.exports = merge(common, {
   mode: "development",
   devtool: "eval-source-map",
-  // output: {
-  //   publicPath: "imageviewer/",
-  // },
   devServer: {
     open: ["/"],
     port: 9020,
     allowedHosts: "all",
     static: [
       {
-        //publicPath: "imageviewer/",
         staticOptions: {
           dotfiles: "allow",
         },
