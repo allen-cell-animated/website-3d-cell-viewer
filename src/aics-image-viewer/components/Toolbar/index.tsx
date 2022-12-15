da2556d52ad7ac078e1c503ff4562a6bbc796752
Cameron Fraser <cameron.fraser@alleninstitute.org>
2022-12-15 12:04:23 -0800

    add toolbar scroll buttons

--- src/aics-image-viewer/components/Toolbar/index.tsx
+++ src/aics-image-viewer/components/Toolbar/index.tsx
@@ -80,13 +80,14 @@ export default function Toolbar(props: ToolbarProps): React.ReactElement {
     checkSize();
   }, []); // Dependency-free effect will only run on mount
 
+  const scrollX = (amount: number): number => (barRef.current!.scrollLeft += amount);
   // Translate vertical scrolling into horizontal scrolling
-  const scrollX: WheelEventHandler<HTMLDivElement> = (e) => {
+  const scrollHandler: WheelEventHandler<HTMLDivElement> = (e) => {
     e.preventDefault();
     if (e.deltaY === 0) {
       return;
     }
-    barRef.current!.scrollLeft += e.deltaY;
+    scrollX(e.deltaY);
   };
 
   const twoDMode = props.mode !== ViewMode.threeD;
@@ -106,97 +107,105 @@ export default function Toolbar(props: ToolbarProps): React.ReactElement {
     "ant-btn-icon-only btn-borderless" + (active ? " btn-active" : "");
 
   return (
-    <div className={`viewer-toolbar${scrollMode ? " viewer-toolbar-scroll" : ""}`} ref={barRef} onWheel={scrollX}>
-      <span className="viewer-toolbar-left" ref={leftRef} />
-      <span className="viewer-toolbar-center" ref={centerRef}>
-        {renderGroup1 && (
-          <span className="viewer-toolbar-group">
-            {renderConfig.viewModeRadioButtons && (
-              <ViewModeRadioButtons mode={props.mode} onViewModeChange={props.onViewModeChange} />
-            )}
-            {renderConfig.resetCameraButton && (
-              <Tooltip placement="bottom" title="Reset camera">
-                <Button className="ant-btn-icon-only btn-borderless" onClick={props.onResetCamera}>
-                  <ViewerIcon type="resetView" />
-                </Button>
-              </Tooltip>
-            )}
-            {renderConfig.autoRotateButton && (
-              <Tooltip placement="bottom" title={turntableToggleTitle}>
-                <Button
-                  className={classForToggleBtn(autorotate && !twoDMode)}
-                  disabled={twoDMode || props.pathTraceOn}
-                  onClick={props.onAutorotateChange}
-                >
-                  <ViewerIcon type="turnTable" />
-                </Button>
-              </Tooltip>
-            )}
-          </span>
-        )}
+    <div className={`viewer-toolbar-container${scrollMode ? " viewer-toolbar-scroll" : ""}`}>
+      <div className="viewer-toolbar-scroll-btn scroll-btn-left" onClick={() => scrollX(-100)}>
+        <ViewerIcon type="closePanel" style={{ fontSize: "12px", transform: "rotate(180deg)" }} />
+      </div>
+      <div className="viewer-toolbar" ref={barRef} onWheel={scrollHandler}>
+        <span className="viewer-toolbar-left" ref={leftRef} />
+        <span className="viewer-toolbar-center" ref={centerRef}>
+          {renderGroup1 && (
+            <span className="viewer-toolbar-group">
+              {renderConfig.viewModeRadioButtons && (
+                <ViewModeRadioButtons mode={props.mode} onViewModeChange={props.onViewModeChange} />
+              )}
+              {renderConfig.resetCameraButton && (
+                <Tooltip placement="bottom" title="Reset camera">
+                  <Button className="ant-btn-icon-only btn-borderless" onClick={props.onResetCamera}>
+                    <ViewerIcon type="resetView" />
+                  </Button>
+                </Tooltip>
+              )}
+              {renderConfig.autoRotateButton && (
+                <Tooltip placement="bottom" title={turntableToggleTitle}>
+                  <Button
+                    className={classForToggleBtn(autorotate && !twoDMode)}
+                    disabled={twoDMode || props.pathTraceOn}
+                    onClick={props.onAutorotateChange}
+                  >
+                    <ViewerIcon type="turnTable" />
+                  </Button>
+                </Tooltip>
+              )}
+            </span>
+          )}
 
-        {renderConfig.fovCellSwitchControls && props.hasCellId && props.hasParentImage && (
-          <span className="viewer-toolbar-group">
-            <Radio.Group value={props.imageType} onChange={({ target }) => props.onSwitchFovCell(target.value)}>
-              <Radio.Button value={ImageType.segmentedCell}>Single cell</Radio.Button>
-              <Radio.Button value={ImageType.fullField}>Full field</Radio.Button>
-            </Radio.Group>
-          </span>
-        )}
+          {renderConfig.fovCellSwitchControls && props.hasCellId && props.hasParentImage && (
+            <span className="viewer-toolbar-group">
+              <Radio.Group value={props.imageType} onChange={({ target }) => props.onSwitchFovCell(target.value)}>
+                <Radio.Button value={ImageType.segmentedCell}>Single cell</Radio.Button>
+                <Radio.Button value={ImageType.fullField}>Full field</Radio.Button>
+              </Radio.Group>
+            </span>
+          )}
 
-        <span className="viewer-toolbar-group">
-          <Select
-            className="select-render-setting"
-            dropdownClassName="viewer-toolbar-dropdown"
-            value={props.renderSetting}
-            onChange={props.onChangeRenderingAlgorithm}
-          >
-            <Select.Option value={RenderMode.volumetric} key={RenderMode.volumetric}>
-              Volumetric
-            </Select.Option>
-            {props.canPathTrace && (
-              <Select.Option value={RenderMode.pathTrace} key={RenderMode.pathTrace} disabled={twoDMode}>
-                Path trace
+          <span className="viewer-toolbar-group">
+            <Select
+              className="select-render-setting"
+              dropdownClassName="viewer-toolbar-dropdown"
+              value={props.renderSetting}
+              onChange={props.onChangeRenderingAlgorithm}
+            >
+              <Select.Option value={RenderMode.volumetric} key={RenderMode.volumetric}>
+                Volumetric
               </Select.Option>
-            )}
-            <Select.Option value={RenderMode.maxProject} key={RenderMode.maxProject}>
-              Max project
-            </Select.Option>
-          </Select>
+              {props.canPathTrace && (
+                <Select.Option value={RenderMode.pathTrace} key={RenderMode.pathTrace} disabled={twoDMode}>
+                  Path trace
+                </Select.Option>
+              )}
+              <Select.Option value={RenderMode.maxProject} key={RenderMode.maxProject}>
+                Max project
+              </Select.Option>
+            </Select>
+          </span>
+
+          {renderGroup4 && (
+            <span className="viewer-toolbar-group">
+              {renderConfig.showAxesButton && (
+                <Tooltip placement="bottom" title={axesToggleTitle}>
+                  <Button className={classForToggleBtn(showAxes)} onClick={toggleAxis}>
+                    <ViewerIcon type="axes" />
+                  </Button>
+                </Tooltip>
+              )}
+              {renderConfig.showBoundingBoxButton && (
+                <Tooltip placement="bottom" title={boundingBoxToggleTitle}>
+                  <Button className={classForToggleBtn(showBoundingBox)} onClick={toggleBoundingBox}>
+                    <ViewerIcon type="boundingBox" />
+                  </Button>
+                </Tooltip>
+              )}
+            </span>
+          )}
         </span>
 
-        {renderGroup4 && (
-          <span className="viewer-toolbar-group">
-            {renderConfig.showAxesButton && (
-              <Tooltip placement="bottom" title={axesToggleTitle}>
-                <Button className={classForToggleBtn(showAxes)} onClick={toggleAxis}>
-                  <ViewerIcon type="axes" />
-                </Button>
-              </Tooltip>
-            )}
-            {renderConfig.showBoundingBoxButton && (
-              <Tooltip placement="bottom" title={boundingBoxToggleTitle}>
-                <Button className={classForToggleBtn(showBoundingBox)} onClick={toggleBoundingBox}>
-                  <ViewerIcon type="boundingBox" />
-                </Button>
-              </Tooltip>
-            )}
-          </span>
-        )}
-      </span>
-
-      <span className="viewer-toolbar-right viewer-toolbar-group" ref={rightRef}>
-        <Tooltip placement="bottom" title="Download">
-          <DownloadButton
-            cellDownloadHref={props.cellDownloadHref}
-            fovDownloadHref={props.fovDownloadHref}
-            hasFov={props.hasCellId && props.hasParentImage}
-          />
-        </Tooltip>
-        <Tooltip placement="bottom" title="Screenshot">
-          <Button icon="camera" className="btn-borderless" onClick={props.downloadScreenshot} />
-        </Tooltip>
-      </span>
+        <span className="viewer-toolbar-right viewer-toolbar-group" ref={rightRef}>
+          <Tooltip placement="bottom" title="Download">
+            <DownloadButton
+              cellDownloadHref={props.cellDownloadHref}
+              fovDownloadHref={props.fovDownloadHref}
+              hasFov={props.hasCellId && props.hasParentImage}
+            />
+          </Tooltip>
+          <Tooltip placement="bottom" title="Screenshot">
+            <Button icon="camera" className="btn-borderless" onClick={props.downloadScreenshot} />
+          </Tooltip>
+        </span>
+      </div>
+      <div className="viewer-toolbar-scroll-btn scroll-btn-right" onClick={() => scrollX(100)}>
+        <ViewerIcon type="closePanel" style={{ fontSize: "12px" }} />
+      </div>
     </div>
   );
 }
