739e8d649eabcedc7dd0bd7bfef41d8ca2939244
Cameron Fraser <cameron.fraser@alleninstitute.org>
2023-02-10 18:21:10 -0800

    solve the near-intractable problem of proper borders, hackily

--- src/aics-image-viewer/components/MetadataViewer/index.tsx
+++ src/aics-image-viewer/components/MetadataViewer/index.tsx
@@ -3,28 +3,38 @@ import React from "react";
 import { MetadataEntry, MetadataRecord } from "../../shared/types";
 import "./styles.css";
 
-interface MetadataTableProps {
+interface MetadataProps {
   metadata: MetadataRecord;
+}
+
+interface MetadataTableProps extends MetadataProps {
+  // track whether categories will abut a category below when closed, for rendering borders properly
+  categoryFollows: boolean;
 }
 
 interface CollapsibleCategoryProps extends MetadataTableProps {
   title: string;
-  className?: string;
 }
 
-export interface MetadataViewerProps extends Partial<MetadataTableProps> {
+export interface MetadataViewerProps extends Partial<MetadataProps> {
   getExtraMetadata?: () => MetadataRecord;
 }
 
 const isCategory = (val: MetadataEntry): val is MetadataRecord => typeof val === "object" && val !== null;
 
 /** Component to hold collapse state */
-const MetadataCollapsibleCategory: React.FC<CollapsibleCategoryProps> = ({ metadata, title }) => {
+const MetadataCollapsibleCategory: React.FC<CollapsibleCategoryProps> = ({ metadata, title, categoryFollows }) => {
   const [collapsed, setCollapsed] = React.useState(true);
+  const toggleCollapsed = () => setCollapsed(!collapsed);
 
   return (
     <>
-      <tr className="metadata-collapse-title" onClick={() => setCollapsed(!collapsed)}>
+      <tr
+        className={
+          "metadata-collapse-title" + (categoryFollows && collapsed ? " metadata-collapse-no-bottom-border" : "")
+        }
+        onClick={toggleCollapsed}
+      >
         <td className="metadata-collapse-caret">
           <Icon type="right" style={{ transform: `rotate(${collapsed ? 0 : 90}deg)` }} />
         </td>
@@ -32,38 +42,58 @@ const MetadataCollapsibleCategory: React.FC<CollapsibleCategoryProps> = ({ metad
       </tr>
       <tr className={"metadata-collapse-content-row" + (collapsed ? " metadata-collapse-collapsed" : "")}>
         <td className="metadata-collapse-content" colSpan={3}>
-          <MetadataTable metadata={metadata} />
+          <MetadataTable metadata={metadata} categoryFollows={categoryFollows} />
         </td>
       </tr>
     </>
   );
 };
 
-const MetadataTable: React.FC<MetadataTableProps> = ({ metadata }) => (
-  <table className="viewer-metadata-table">
-    <tbody>
-      {Object.keys(metadata).map((key, idx) => {
-        const metadataValue = metadata[key];
-        if (isCategory(metadataValue)) {
-          return <MetadataCollapsibleCategory key={idx} metadata={metadataValue} title={key} />;
-        } else {
-          return (
-            <tr key={idx}>
-              <td className="metadata-key" colSpan={2}>
-                {key}
-              </td>
-              <td className="metadata-value">{metadataValue}</td>
-            </tr>
-          );
-        }
-      })}
-    </tbody>
-  </table>
-);
+const MetadataTable: React.FC<MetadataTableProps> = ({ metadata, categoryFollows }) => {
+  const metadataKeys = Object.keys(metadata);
+
+  return (
+    <table className="viewer-metadata-table">
+      <tbody>
+        {metadataKeys.map((key, idx) => {
+          const metadataValue = metadata[key];
+
+          if (isCategory(metadataValue)) {
+            let categoryBelow: boolean;
+            if (idx + 1 >= metadataKeys.length) {
+              categoryBelow = categoryFollows;
+            } else {
+              categoryBelow = isCategory(metadata[metadataKeys[idx + 1]]);
+              console.log(key, metadataKeys[idx + 1]);
+            }
+
+            return (
+              <MetadataCollapsibleCategory
+                key={idx}
+                metadata={metadataValue}
+                title={key}
+                categoryFollows={categoryBelow}
+              />
+            );
+          } else {
+            return (
+              <tr key={idx}>
+                <td className="metadata-key" colSpan={2}>
+                  {key}
+                </td>
+                <td className="metadata-value">{metadataValue}</td>
+              </tr>
+            );
+          }
+        })}
+      </tbody>
+    </table>
+  );
+};
 
 const MetadataViewer: React.FC<MetadataViewerProps> = ({ metadata, getExtraMetadata }) => {
   const extraMetadata = getExtraMetadata ? getExtraMetadata() : {};
-  return <MetadataTable metadata={{ ...extraMetadata, ...metadata }} />;
+  return <MetadataTable metadata={{ ...extraMetadata, ...metadata }} categoryFollows={false} />;
 };
 
 export default MetadataViewer;
