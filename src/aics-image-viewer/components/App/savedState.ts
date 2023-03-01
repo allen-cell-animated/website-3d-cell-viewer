3aafbfcaf817d792e4b6f50d1d6d0b39167d96de
Cameron Fraser <cameron.fraser@alleninstitute.org>
2023-03-01 12:55:55 -0800

    restructure `SavedState` a bit

--- src/aics-image-viewer/components/App/savedState.ts
+++ src/aics-image-viewer/components/App/savedState.ts
@@ -1,23 +1,24 @@
+import { ChannelState } from "../../shared/utils/viewerChannelSettings";
 import { RenderConfig, UserSelectionState } from "./types";
 
 interface View3dInternalState {
   pixelSamplingRate: number;
-  exposure: number;
-  fov: number;
-  flipVolume: [number, number, number];
   stepSizePrimaryRayVoxels: number;
   stepSizeSecondaryRayVoxels: number;
-  // wait - is this per-channel?
-  specular: [number, number, number][];
-  emissive: [number, number, number][];
-  glossiness: number[];
+  flipVolume: [number, number, number];
+}
+
+interface View3dChannelState {
+  specular: [number, number, number];
+  emissive: [number, number, number];
+  glossiness: number;
 }
 
 // TODO: how to create a type error when one of this type's dependent interfaces
 // (RenderConfig, UserSelectionState, etc.) changes, to signal the need for a
 // new version and a converter from the old version?
 export interface SavedState {
-  stateVersion: number;
+  stateVersion: 1;
 
   baseUrl: string;
   cellId: string;
@@ -29,8 +30,11 @@ export interface SavedState {
   // Which UI elements are rendered
   renderConfig: RenderConfig;
   // Global (not per-channel) viewer settings which may be changed in the UI
-  viewerConfig: UserSelectionState;
+  viewerConfig: UserSelectionState & View3dInternalState;
   // State which exists in view3d but is not controlled by this component
-  view3dInternalState: View3dInternalState;
-  // TODO: camera, per-channel settings
+  // view3dInternalState: View3dInternalState;
+  // TODO TODO TODO
+  // Per-channel settings
+  channelSettings: (ChannelState & View3dChannelState)[];
+  // TODO: camera
 }
