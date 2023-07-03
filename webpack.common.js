174a65c2db0e44fec4e1e58746fcd58da20e6e00
toloudis <danielt@alleninstitute.org>
2023-07-03 10:05:30 -0700

    remove APP_VERSION define which does not seem to be used

--- webpack.common.js
+++ webpack.common.js
@@ -29,18 +29,15 @@ module.exports = (env) => {
       }),
       new MiniCssExtractPlugin(),
       new webpack.DefinePlugin({
-        APP_VERSION: JSON.stringify(require("./package.json").version),
+        WEBSITE3DCELLVIEWER_VERSION: JSON.stringify(require("./package.json").version),
+        VOLUMEVIEWER_VERSION: JSON.stringify(require("./node_modules/@aics/volume-viewer/package.json").version),
+        WEBSITE3DCELLVIEWER_BUILD_ENVIRONMENT: JSON.stringify(env.env),
       }),
       new CopyWebpackPlugin({ patterns: ["./.nojekyll"] }),
       new webpack.ProvidePlugin({
         THREE: "three",
         jQuery: "jquery",
         $: "jquery",
-      }),
-      new webpack.DefinePlugin({
-        WEBSITE3DCELLVIEWER_VERSION: JSON.stringify(require("./package.json").version),
-        VOLUMEVIEWER_VERSION: JSON.stringify(require("./node_modules/@aics/volume-viewer/package.json").version),
-        WEBSITE3DCELLVIEWER_BUILD_ENVIRONMENT: JSON.stringify(env.env),
       }),
     ],
     module: {
