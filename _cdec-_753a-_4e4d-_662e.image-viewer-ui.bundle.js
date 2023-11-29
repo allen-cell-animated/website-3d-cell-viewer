05a8a01aa075bdbeb2239bdb19e321e21a7c66cd
toloudis <toloudis@users.noreply.github.com>
2023-11-29 00:06:30 +0000

    deploy: 6ccf453b8356275a8dd3798caacd7defc37cf0d4

--- _cdec-_753a-_4e4d-_662e.image-viewer-ui.bundle.js
+++ _cdec-_753a-_4e4d-_662e.image-viewer-ui.bundle.js
@@ -210,10 +210,13 @@
 /******/ 		var document = __webpack_require__.g.document;
 /******/ 		if (!scriptUrl && document) {
 /******/ 			if (document.currentScript)
-/******/ 				scriptUrl = document.currentScript.src
+/******/ 				scriptUrl = document.currentScript.src;
 /******/ 			if (!scriptUrl) {
 /******/ 				var scripts = document.getElementsByTagName("script");
-/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 				if(scripts.length) {
+/******/ 					var i = scripts.length - 1;
+/******/ 					while (i > -1 && !scriptUrl) scriptUrl = scripts[i--].src;
+/******/ 				}
 /******/ 			}
 /******/ 		}
 /******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
