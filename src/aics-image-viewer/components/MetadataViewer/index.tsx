f39c2aaf57c4a44b0522704c0ee1504b432ad6a2
Cameron Fraser <cameron.fraser@alleninstitute.org>
2023-12-12 18:52:24 -0800

    there was an easier way to do borders this whole time...

--- src/aics-image-viewer/components/MetadataViewer/index.tsx
+++ src/aics-image-viewer/components/MetadataViewer/index.tsx
@@ -5,11 +5,6 @@ import "./styles.css";
 
 interface MetadataTableProps {
   metadata: MetadataRecord;
-  // Track whether a category title will abut another category title below when in the collapsed state.
-  // If so, this category title should not render a bottom border when collapsed, otherwise the border will double
-  // up with the top border of the lower category and create the appearance of a single thick, uneven border.
-  // (there is not a way to prevent this with pure CSS to my knowledge)
-  categoryFollows: boolean;
   topLevel?: boolean;
 }
 
@@ -38,16 +33,13 @@ const sortCategoriesFirst = (entry: MetadataEntry): MetadataEntry => {
 };
 
 /** Component to hold collapse state */
-const MetadataCategory: React.FC<CollapsibleCategoryProps> = ({ metadata, title, categoryFollows }) => {
+const MetadataCategory: React.FC<CollapsibleCategoryProps> = ({ metadata, title }) => {
   const [collapsed, setCollapsed] = React.useState(true);
-  const toggleCollapsed = (): void => setCollapsed(!collapsed);
-
-  const rowClass =
-    (collapsed ? " metadata-collapse-collapsed" : "") + (!categoryFollows ? " metadata-collapse-bottom-border" : "");
+  const collapsedClass = collapsed ? " metadata-collapse-collapsed" : "";
 
   return (
     <>
-      <tr className={"metadata-collapse-title" + rowClass} onClick={toggleCollapsed}>
+      <tr className={"metadata-row-collapse-title" + collapsedClass} onClick={() => setCollapsed(!collapsed)}>
         <td colSpan={2}>
           <span className="metadata-collapse-caret">
             <Icon type="right" style={{ transform: `rotate(${collapsed ? 0 : 90}deg)` }} />
@@ -55,16 +47,16 @@ const MetadataCategory: React.FC<CollapsibleCategoryProps> = ({ metadata, title,
           {title}
         </td>
       </tr>
-      <tr className={"metadata-collapse-content-row" + rowClass}>
+      <tr className={"metadata-row-collapse-content" + collapsedClass}>
         <td className="metadata-collapse-content" colSpan={2}>
-          <MetadataTable metadata={metadata} categoryFollows={categoryFollows} />
+          <MetadataTable metadata={metadata} />
         </td>
       </tr>
     </>
   );
 };
 
-const MetadataTable: React.FC<MetadataTableProps> = ({ metadata, categoryFollows, topLevel }) => {
+const MetadataTable: React.FC<MetadataTableProps> = ({ metadata, topLevel }) => {
   const metadataKeys = Object.keys(metadata);
   const metadataIsArray = Array.isArray(metadata);
 
@@ -75,13 +67,7 @@ const MetadataTable: React.FC<MetadataTableProps> = ({ metadata, categoryFollows
           const metadataValue = sortCategoriesFirst(metadataIsArray ? metadata[idx] : metadata[key]);
 
           if (isCategory(metadataValue)) {
-            // Determine whether this category is followed by another category, ignoring data hierarchy:
-            // - If this is the last element in the table, this category has another category below if the table does.
-            // - Otherwise, just check if the next element in this table is a category.
-            const nextItem = metadataIsArray ? metadata[idx + 1] : metadata[metadataKeys[idx + 1]];
-            const categoryBelow = idx + 1 >= metadataKeys.length ? isCategory(nextItem) : categoryFollows;
-
-            return <MetadataCategory key={key} metadata={metadataValue} title={key} categoryFollows={categoryBelow} />;
+            return <MetadataCategory key={key} metadata={metadataValue} title={key} />;
           } else {
             return (
               <tr key={key}>
@@ -97,7 +83,7 @@ const MetadataTable: React.FC<MetadataTableProps> = ({ metadata, categoryFollows
 };
 
 const MetadataViewer: React.FC<{ metadata: MetadataRecord }> = ({ metadata }) => (
-  <MetadataTable metadata={metadata} categoryFollows={false} topLevel={true} />
+  <MetadataTable metadata={metadata} topLevel={true} />
 );
 
 export default MetadataViewer;
