19f41eced085fd5951b99d7d7384f9726ec0054b
Peyton Lee <peyton.lee@alleninstitute.org>
2024-04-08 16:05:25 -0700

    fix: Slightly adjusted video position

--- src/landing-page/components/LandingPage/index.tsx
+++ src/landing-page/components/LandingPage/index.tsx
@@ -50,7 +50,7 @@ const BannerVideoContainer = styled.div`
   & > video {
     width: 100%;
     height: 100%;
-    object-position: 50% 30%;
+    object-position: 50% 40%;
     object-fit: cover;
   }
 `;
