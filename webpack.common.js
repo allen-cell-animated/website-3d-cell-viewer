b8e69a34ca7b9eff0993a3a224b7d3a30748548c
Peyton Lee <peyton.lee@alleninstitute.org>
2024-04-19 08:56:08 -0700

    fix: Removed less plugins, dependencies

--- webpack.common.js
+++ webpack.common.js
@@ -1,16 +1,10 @@
 const path = require("path");
-const fs = require("fs");
 const webpack = require("webpack");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-
-const lessToJs = require("less-vars-to-js");
-const themeVariables = lessToJs(
-  fs.readFileSync(path.join(__dirname, "src/aics-image-viewer/styles/ant-vars.less"), "utf8")
-);
 
 module.exports = (env) => {
   return {
@@ -50,28 +44,6 @@ module.exports = (env) => {
         {
           test: /\.css$/,
           use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
-        },
-        {
-          test: /\.less$/,
-          use: [
-            "style-loader",
-            {
-              loader: "css-loader",
-              options: {
-                importLoaders: 1,
-              },
-            },
-            {
-              loader: "less-loader",
-              options: {
-                lessOptions: {
-                  javascriptEnabled: true,
-                  modifyVars: themeVariables,
-                  math: "always",
-                },
-              },
-            },
-          ],
         },
         {
           test: /\.(woff|woff2|tff|eot|glyph)$/,
