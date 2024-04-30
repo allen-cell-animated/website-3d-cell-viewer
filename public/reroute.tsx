20718dee93be9b3d2b3469828e45bdbf6670f4c7
Peyton Lee <peyton.lee@alleninstitute.org>
2024-04-30 12:30:26 -0700

    fix: Fix reroute by editing base path segment count

--- public/reroute.tsx
+++ public/reroute.tsx
@@ -1,6 +1,9 @@
 import { convertUrlToQueryStringPath } from "../website/utils/route_utils";
 
+// This script is used in the 404.html page to redirect the browser to the correct URL.
 // Convert the current URL to a query string path and redirect the browser.
 const location = window.location;
 const locationUrl = new URL(location.toString());
-location.replace(convertUrlToQueryStringPath(locationUrl));
+const newUrl = convertUrlToQueryStringPath(locationUrl, 1);
+console.log("Redirecting to " + newUrl.toString());
+location.replace(newUrl);
