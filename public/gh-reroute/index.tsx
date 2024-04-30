ad7b8262fdcce7bc4282a0e61eb97f7377ad77d1
Peyton Lee <peyton.lee@alleninstitute.org>
2024-04-30 14:41:48 -0700

    refactor: Style changes to 404 page

--- public/gh-reroute/index.tsx
+++ public/gh-reroute/index.tsx
@@ -1,5 +1,4 @@
 import { convertUrlToQueryStringPath } from "../../website/utils/route_utils";
-import "./styles.css";
 
 // This script is used in the 404.html page to redirect the browser to the correct URL.
 // Convert the current URL to a query string path and redirect the browser.
