fb43c68b691fe757125df681ae7ab8b9e0a0bda6
Peyton Lee <peyton.lee@alleninstitute.org>
2024-04-30 13:40:53 -0700

    test: Fix URL missing basename

--- public/index.tsx
+++ public/index.tsx
@@ -13,16 +13,6 @@ import StyleProvider from "../src/aics-image-viewer/components/StyleProvider";
 import "../src/aics-image-viewer/assets/styles/typography.css";
 import "./App.css";
 
-// Check for redirects in the query string, and update browser history state.
-const locationUrl = new URL(window.location.toString());
-if (isQueryStringPath(locationUrl)) {
-  const url = convertQueryStringPathToUrl(locationUrl);
-
-  const newRelativePath = url.pathname + url.search + url.hash;
-  console.log("Redirecting to " + newRelativePath);
-  window.history.replaceState(null, "", newRelativePath);
-}
-
 // vars filled at build time using webpack DefinePlugin
 console.log(`website-3d-cell-viewer ${WEBSITE3DCELLVIEWER_BUILD_ENVIRONMENT} build`);
 console.log(`website-3d-cell-viewer Version ${WEBSITE3DCELLVIEWER_VERSION}`);
@@ -30,6 +20,18 @@ console.log(`website-3d-cell-viewer Basename ${WEBSITE3DCELLVIEWER_BASENAME}`);
 console.log(`volume-viewer Version ${VOLUMEVIEWER_VERSION}`);
 
 const basename = WEBSITE3DCELLVIEWER_BASENAME;
+
+// Check for redirects in the query string, and update browser history state.
+const locationUrl = new URL(window.location.toString());
+if (isQueryStringPath(locationUrl)) {
+  const url = convertQueryStringPathToUrl(locationUrl);
+  console.log("Converted URL", url);
+
+  const newRelativePath = "/" + basename + url.pathname + url.search + url.hash;
+  console.log("Redirecting to " + newRelativePath);
+  window.history.replaceState(null, "", newRelativePath);
+}
+
 const routes = [
   {
     path: "/",
