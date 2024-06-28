e3dc19ae7c8e2ce068d3f612184743440db04477
Cameron Fraser <cameron.fraser@alleninstitute.org>
2024-06-27 18:37:57 -0700

    set up new colors in basic tf editor

--- src/aics-image-viewer/components/StyleProvider/index.tsx
+++ src/aics-image-viewer/components/StyleProvider/index.tsx
@@ -93,6 +93,7 @@ const theme = {
       text: palette.ltGrey,
       sectionBg: palette.medDarkGrey,
       drawerBg: palette.veryDarkGrey,
+      rampSlider: palette.medPurple,
     },
     toolbar: {
       buttonBg: "#000000cc",
@@ -195,6 +196,7 @@ const CssProvider = styled.div<{ $theme: AppTheme }>`
       --color-controlpanel-text: ${$theme.colors.controlPanel.text};
       --color-controlpanel-section-bg: ${$theme.colors.controlPanel.sectionBg};
       --color-controlpanel-drawer-bg: ${$theme.colors.controlPanel.drawerBg};
+      --color-controlpanel-ramp-slider: ${$theme.colors.controlPanel.rampSlider};
 
       --color-landingpage-bg: ${$theme.colors.landingPage.bg};
       --color-landingpage-text: ${$theme.colors.landingPage.text};
