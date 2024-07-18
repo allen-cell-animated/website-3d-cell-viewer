7f36ea2f6789e843c29bac32dd833765c351998f
Cameron Fraser <cameron.fraser@alleninstitute.org>
2024-07-17 17:00:13 -0700

    make `controlPointsToRamp` a bit more robust

--- src/aics-image-viewer/shared/utils/controlPointsToLut.ts
+++ src/aics-image-viewer/shared/utils/controlPointsToLut.ts
@@ -73,10 +73,12 @@ export function initializeLut(
 }
 
 export function controlPointsToRamp(controlPoints: ControlPoint[]): [number, number] {
-  if (controlPoints.length < 3) {
+  if (controlPoints.length === 1 || controlPoints.length === 3) {
     return [0, TFEDITOR_MAX_BIN];
+  } else if (controlPoints.length === 2) {
+    return [controlPoints[0].x, controlPoints[1].x];
   }
-  return [controlPoints[1].x, controlPoints[2].x];
+  return [controlPoints[1].x, controlPoints[controlPoints.length - 2].x];
 }
 
 export function rampToControlPoints([min, max]: [number, number]): ControlPoint[] {
