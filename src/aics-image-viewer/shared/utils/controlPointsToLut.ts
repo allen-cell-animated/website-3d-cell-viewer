c5fb18240d20aa20b7c659320eef0301f25707d0
Cameron Fraser <cameron.fraser@alleninstitute.org>
2024-07-22 17:20:58 -0700

    use volume-viewer's newly-exported remap function

--- src/aics-image-viewer/shared/utils/controlPointsToLut.ts
+++ src/aics-image-viewer/shared/utils/controlPointsToLut.ts
@@ -1,4 +1,4 @@
-import { Channel, ControlPoint, Lut, Volume } from "@aics/volume-viewer";
+import { Channel, ControlPoint, Lut, Volume, remapControlPoints } from "@aics/volume-viewer";
 import { findFirstChannelMatch, ViewerChannelSettings } from "./viewerChannelSettings";
 import { LUT_MAX_PERCENTILE, LUT_MIN_PERCENTILE } from "../constants";
 import { TFEDITOR_DEFAULT_COLOR, TFEDITOR_MAX_BIN } from "../../components/TfEditor";
@@ -100,8 +100,5 @@ export function remapControlPointsForChannel(
     return controlPoints;
   }
 
-  // TODO: this creates a redundant Uint8Array and algorithmically fills it twice. Can we avoid this?
-  const remapLut = new Lut().createFromControlPoints(controlPoints);
-  remapLut.remapDomains(oldRange[0], oldRange[1], rawMin, rawMax);
-  return remapLut.controlPoints;
+  return remapControlPoints(controlPoints, oldRange[0], oldRange[1], rawMin, rawMax);
 }
