28957c43329d068fe2728e5c1b34a55a313ec65a
Cameron Fraser <cameron.fraser@alleninstitute.org>
2024-09-17 11:25:11 -0700

    functionize "`SharedCheckbox`"

--- src/aics-image-viewer/components/shared/SharedCheckBox.tsx
+++ src/aics-image-viewer/components/shared/SharedCheckBox.tsx
@@ -10,53 +10,20 @@ type SharedCheckboxProps<T> = React.PropsWithChildren<{
   style?: React.CSSProperties;
 }>;
 
-type SharedCheckboxState<T> = {
-  checkedList: T[];
-  indeterminate: boolean;
-  checkAll: boolean;
+const SharedCheckbox = <T,>(props: SharedCheckboxProps<T>): React.ReactNode => {
+  const onCheckAllChange = ({ target }: CheckboxChangeEvent): void => {
+    const { allOptions, onChecked, onUnchecked } = props;
+    target.checked ? onChecked(allOptions) : onUnchecked(allOptions);
+  };
+
+  const indeterminate = !!props.checkedList.length && props.checkedList.length < props.allOptions.length;
+  const checkAll = props.checkedList.length === props.allOptions.length;
+
+  return (
+    <Checkbox indeterminate={indeterminate} onChange={onCheckAllChange} checked={checkAll} style={props.style}>
+      {props.children}
+    </Checkbox>
+  );
 };
 
-export default class SharedCheckbox<T> extends React.Component<SharedCheckboxProps<T>, SharedCheckboxState<T>> {
-  constructor(props: SharedCheckboxProps<T>) {
-    super(props);
-    this.onCheckAllChange = this.onCheckAllChange.bind(this);
-    this.state = {
-      checkedList: props.checkedList,
-      indeterminate: true,
-      checkAll: false,
-    };
-  }
-
-  // TODO: Is this component's derived state strictly necessary? Can some or all of it be removed?
-  static getDerivedStateFromProps<T>(newProps: SharedCheckboxProps<any>): Partial<SharedCheckboxState<T>> {
-    const { checkedList, allOptions } = newProps;
-    return {
-      checkedList,
-      indeterminate: !!checkedList.length && checkedList.length < allOptions.length,
-      checkAll: checkedList.length === allOptions.length,
-    };
-  }
-
-  onCheckAllChange({ target }: CheckboxChangeEvent): void {
-    const { allOptions, onChecked, onUnchecked } = this.props;
-    target.checked ? onChecked(allOptions) : onUnchecked(allOptions);
-    this.setState({
-      checkedList: target.checked ? allOptions : [],
-      indeterminate: false,
-      checkAll: target.checked,
-    });
-  }
-
-  render(): React.ReactNode {
-    return (
-      <Checkbox
-        indeterminate={this.state.indeterminate}
-        onChange={this.onCheckAllChange}
-        checked={this.state.checkAll}
-        style={this.props.style}
-      >
-        {this.props.children}
-      </Checkbox>
-    );
-  }
-}
+export default SharedCheckbox;
