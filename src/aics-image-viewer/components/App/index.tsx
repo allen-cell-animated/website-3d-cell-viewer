111baefe76f42b26326ef87db2e46425835803e1
dmt <dmt@completelydifferent.com>
2024-10-27 11:06:56 -0700

    comment

--- src/aics-image-viewer/components/App/index.tsx
+++ src/aics-image-viewer/components/App/index.tsx
@@ -166,6 +166,7 @@ const App: React.FC<AppProps> = (props) => {
   );
 
   // install loadContext into view3d
+  // TODO UNCOMMENT THIS AFTER VOLUME-VIEWER 3.11.3+ IS RELEASED
   //view3d.loaderContext = loadContext;
 
   const loader = useRef<IVolumeLoader>();
